c3a46a80301dcb1892b130688034693c5cdf015e
Janko Veselinovic <84241580+JankoTheHuman@users.noreply.github.com>
2024-01-21 10:33:07 +0100

    Add files via upload

---
+++ Bobicasti Raj/js/myJs.js
@@ -0,0 +1,30 @@
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'albumLabel': 'Slika %1 od %2'
+})
+
+
+ function prikazi(jagoda){
+
+    
+
+    kontent = document.getElementById('kontent');
+
+
+    if(jagoda == 'albion'){
+        kontent.innerHTML = "<img src='images/albion.jpg' height='400' class='card-img-top slikaJagoda' alt='Albion'>        <div class='card-body'>            <h1 class='card-title slovo'>Albion Jagoda</h1>            <hr>            <p class='card-text'>&nbsp;&nbsp;&nbsp;&nbsp;Albion jagoda potiče iz Sjedinjenih Američkih Država. Razvijena je na                Univerzitetu Kalifornije, a prvi put je predstavljena 2004. godine.                Ova sorta jagode obično ima snažan rast i može se uzgajati kao jednogodišnja ili                višegodišnja biljka. Biljke su često srednje visine i pružaju dobar prinos plodova.                Albion jagode su poznate po krupnim, čvrstim i sočnim plodovima. Imaju slatkast ukus i                aromu, što ih čini popularnim izborom među ljubiteljima jagoda.                Berba Albion jagoda obično počinje u sredinom do kasnog proleća i traje tokom leta. Ova                sorta je poznata po produženom periodu berbe.                Albion jagoda je prilagodljiva sorta koja se može uzgajati u različitim klimatskim                uslovima, ali uspeva najbolje u toplijim podnebljima.</p>        </div>"
+    }else if(jagoda == 'irma'){
+        kontent.innerHTML = "<img src='images/irma.jpg' height='400' class='card-img-top slikaJagoda' alt='Irma'>                        <div class='card-body'>                            <h1 class='card-title slovo'>Irma Jagoda</h1>                            <hr>                            <p class='card-text'>&nbsp;&nbsp;&nbsp;&nbsp;Irma ima krupne slatke crvene plodove sjajnog ukusa i mirisa. Moze se saditi u saksijama,&nbsp;zardinjerama,&nbsp;u nekim zanimljivim posudama,&nbsp;plasteniku ili basti. Radjaju od pocetka maja pa do prvih snegova. Laka za uzgoj otporna na bolesti. Irma je cenjena zbog nežnosti i mirisa. Teško je pronaći popularniju sortu jagoda,&nbsp;jer plodovi narastu dovoljno veliki i daju visok prinos,&nbsp;a briga o njoj je minimalna. Plodovi su atraktivni,&nbsp;krupni,&nbsp;izduženi,&nbsp;blistave krv crvene boje,&nbsp;izražene slatkoće,&nbsp;složenog i višestrukog ukusa zbog jedva primetne kiselosti.</p>                        </div>"
+    }else if(jagoda == 'sumske'){
+        kontent.innerHTML = "<img src='images/sumska.jpg' height='400' class='card-img-top slikaJagoda' alt='Šumska'>                        <div class='card-body'>                            <h1 class='card-title slovo'>Šumska Jagoda</h1>                            <hr>                            <p class='card-text'>&nbsp;&nbsp;&nbsp;&nbsp;Šumska jagoda je često poznata i kao divlja jagoda. Divlje jagode često rastu u šumama,&nbsp;šikarama i na ivicama polja. Različite vrste divljih jagoda mogu se naći širom sveta. Divlje jagode obično imaju manje plodove u poređenju sa komercijalnim sortama jagoda,&nbsp;ali su poznate po intenzivnom ukusu i aromi. Biljke divljih jagoda obično su prilagođene rastu u divljim,&nbsp;prirodnim okruženjima. Plodovi divljih jagoda su manji u veličini,&nbsp;ali su često bogati aromom i ukusom. Boja plodova može varirati od crvene do tamnocrvene,&nbsp;zavisno o vrsti. Divlje jagode obično dozrevaju tokom letnjih meseci,&nbsp;a berba se vrši kada plodovi postanu zreli. Divlje jagode se ponekad koriste za prirodnu ishranu ili u pravljenju džemova i drugih proizvoda,&nbsp;ali često nisu toliko velike ili otporne kao komercijalne sorte jagoda koje se uzgajaju u poljoprivrednim sistemima.  </div>"
+    }else if(jagoda == 'ananas'){
+        kontent.innerHTML = "<img src='images/ananas.jpg' height='400' class='card-img-top slikaJagoda' alt='Ananas'>                        <div class='card-body'>                            <h1 class='card-title slovo'>Bela Ananas Jagoda</h1>                            <hr>                            <p class='card-text'>&nbsp;&nbsp;&nbsp;&nbsp;Ananas jagode su uglavnom nešto sitnije od crvenih domaćih,&nbsp;bele su i imaju na sebi crvene tačke. U slučaju da prezru dobijaju rozikastu boju. Imaju miris šumskih jagoda,&nbsp;a ukus ananasa. U Južnoj Americi se dugo proizvode,&nbsp;a holandski farmeri su ih doneli na svoje tržište početkom decenije,&nbsp;odakle su se proširile i na Srbiju.Glavna razlika između crvenih i belih jagoda,&nbsp;ukoliko bele ne nastaju ukrštanjem crvenih jagoda i ananasa,&nbsp;je u proteinu Fra a1,&nbsp;na koji su neki ljudi alergični,&nbsp;a u belim jagodama ih uopšte i nema.Veoma su lake za gajenje,&nbsp;ne zahtevaju nikakav poseban tretman,&nbsp;osim što ih treba štititi od sunca jer ne podnose preveliku toplotu,&nbsp;a ni svetlost,&nbsp;te se mogu gajiti i u stakleniku,&nbsp;što je čak i poželjnije. Ovo sakrivanje od svetlosti smanjuje i nivo antocijana,&nbsp;prirodne hemikalije u voću i povrću koja mu daje boju. Bele jagode se ne prskaju i,&nbsp;u slučaju da ste se pitali,&nbsp;nisu GMO,&nbsp;odnosno genetski modifikovane.   </div>"
+    }
+    
+        
+        
+ }
+
+
+ 
